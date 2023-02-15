8fae4f8e5350d131e8736289207d319de5f1db90
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-02-15 19:10:06 +0200

    update repo

--- server/app.js
+++ server/app.js
@@ -7,6 +7,15 @@ const PostRouter = require("./routes/Posts.js")
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'none'; img-src 'self' https://fullstack-blog-app.onrender.com"
+  );
+  next();
+});
+
 app.use(
   cors({
     origin: [
