4fe63ece15e572eca792f50fb95ac7ad9ef3ddf1
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-02-15 13:38:20 +0200

    update repo

--- server/app.js
+++ server/app.js
@@ -7,7 +7,9 @@ const PostRouter = require("./routes/Posts.js")
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://fullstack-blog-app.onrender.com"]
+}));
 dotenv.config();
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
